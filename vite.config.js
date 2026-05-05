2887dfbc4a248138f21044bb4bcfcb3de85ab5a2
Ricardo Miranda <Dev.rm.f@outlook.com>
2026-05-05 15:07:13 -0600

    resolviendo errores commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'                   
+import { fileURLToPath } from 'url'              
+import { dirname } from 'path'  
 
 export default defineConfig({
   plugins: [
